6e778b24af91c7bd7fa8ad09645059af43a61220
Tomke <pfoch.tomke@gmail.com>
2024-08-05 16:34:09 +0200

    serial

--- Arduino.hpp
+++ Arduino.hpp
@@ -1,136 +1,41 @@
-#include <bits/chrono.h>
-#include <cmath>
+#pragma once
 #include <cstdint>
-#include <iostream>
-#include <ctime>
+#include <vector>
+#include "CWM/CWM.h"
+#include <string>
 
 
-auto start_time = std::chrono::steady_clock::now();
-
-long millis()
-{
-        auto now = std::chrono::steady_clock::now();
-        return  std::chrono::duration_cast<std::chrono::nanoseconds>(now-start_time).count()/1000000;
-}
-
-class Serial{
-public:
-        static void begin(int _)
-        {}
-        template<typename ...Tp>
-        static void print(Tp... args)
-        {
-                ((std::cout << args), ...);
-        }
-        template<typename ...Tp>
-        static void println(Tp... args)
-        {
-            Serial::print(args...);
-            std::cout << std::endl;
-        }
-
-};
-
-Serial Serial;
-
-uint8_t pins[30]= {0};
-
-void analogWrite(int pin, uint8_t value){
-        pins[pin]=value;
-}
-
-uint8_t analogRead(int pin){
-        return pins[pin];
-}
-
-bool digitalRead(int pin){
-        return pins[pin];
-}
-
-void digitalWrite(int pin, bool value){
-        pins[pin]=value;
-}
+//Arduino
 #define OUTPUT 1
 #define INPUT 0
 #define LOW 0
 #define HIGH 1
 #define A0 4
-void pinMode(int pin, int mode){}
+using String = std::string;
 
-class MotorEmu{
-        int enable,a,b;
-        double rot=0;
-        double vel=0;
-        double accel=0.5;
-        double deccel=1.00000001;
-        double last_millis=0;
-public:
-        MotorEmu(int enable, int a, int b)
-        : enable(enable), a(a), b(b)
-        {}
-        void update(){
-                double time = millis(),
-                       delta = time-last_millis;
-                last_millis = time;
+long millis();
 
-                int direction = digitalRead(a)-digitalRead(b);
-
-                vel+=accel*sqrt(analogRead(enable))*direction*delta/1000.0;
-
-                vel-=deccel*vel*delta/1000.0;
-                rot+=vel*delta/1000.0;
-        }
-        double getRot(){
-                return rot;
-        }
-
-        float getVel(){
-                return vel;
-        }
-
-        float getRPM(){
-                return vel*60.0/2.0/3.141;
-        }
-
-};
-
-class PhotoEmu
-{
-        MotorEmu &m;
-        int pin;
-public:
-        PhotoEmu(int pin, MotorEmu &m)
-        : m(m), pin(pin)
-        {}
-
-        void update()
-        {
-                // Serial.println(m.getRot(), " : ", std::abs(std::sin(m.getRot()))>0.9);
-                analogWrite(pin,(std::abs(std::sin(m.getRot()))>0.8)*25);
-        }
-};
+void analogWrite(int pin, uint8_t value);
+uint8_t analogRead(int pin);
+bool digitalRead(int pin);
+void digitalWrite(int pin, bool value);
+void pinMode(int pin, int mode);
 
 void setup();
 void loop();
 
+class Serial;
+extern Serial Serial;
 
 
-int main(void){
-        MotorEmu me(1,2,3);
-        PhotoEmu ph(4, me);
-        digitalWrite(2, 1);
-        digitalWrite(3, 0);
-        analogWrite(1, 100);
+// Emulators
+class Module
+{
+        public:
+                const std::string name="No Name";
+                virtual void update()=0;
+                virtual std::vector<std::pair<std::string, std::string>> getAttributes(){ return {};}
+                virtual std::vector<CWM_window> getWindows(){ return {};}
+};
 
-        setup();
-        while(true){
-                me.update();
-                ph.update();
-
-                if((int)millis()%50==0){
-                        Serial.print((int)millis(), ": ");
-                        Serial.print(me.getRPM(), " ");
-                }
-                loop();
-        }
-}
+std::vector<std::shared_ptr<Module>> setupEmulator();
